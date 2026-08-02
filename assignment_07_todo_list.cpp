5e4c0d6e76e8538363d724c1f3706b379769b58a
nanayaa-tech <nanasally219@gmail.com>
2026-08-02 23:34:51 +0000

    Completed assignment 07

--- assignment_07_todo_list.cpp
+++ assignment_07_todo_list.cpp
@@ -80,3 +80,95 @@
 #include <string>
 using namespace std;
 
+void addTask(vector<string>& tasks)
+{
+  string task; 
+
+  cin.ignore();
+  cout << "Enter task: ";
+  getline(cin,task);
+
+  tasks.push_back(task);
+  cout << "Task added: \"" << task << "\"" << endl;
+}
+
+void viewTasks(const vector<string>& tasks)
+{
+  if (tasks.empty())
+  {
+    cout << "Your to-do list is empty." << endl;
+    return;
+  }
+  cout << "\nYour Tasks:\n";
+  for (int i = 0; i < tasks.size(); i++)
+    {
+        cout << i + 1 << ". " << tasks[i] << endl;
+    }
+}
+
+// Function to delete a task
+void deleteTask(vector<string>& tasks)
+{
+    if (tasks.empty())
+    {
+        cout << "There are no tasks to delete." << endl;
+        return;
+    }
+
+    viewTasks(tasks);
+
+    int choice;
+
+    cout << "Enter task number to delete: ";
+    cin >> choice;
+
+    if (choice < 1 || choice > tasks.size())
+    {
+        cout << "Invalid task number." << endl;
+        return;
+    }
+
+    cout << "Task \"" << tasks[choice - 1] << "\" has been removed." << endl;
+
+    tasks.erase(tasks.begin() + choice - 1);
+}
+
+int main()
+{
+    vector<string> tasks;
+    int choice;
+    do
+  { 
+    cout << "\n============================\n";
+    cout << "      TO-DO LIST MENU\n";
+    cout << "============================\n";
+    cout << "1. Add task\n";
+    cout << "2. View tasks\n";
+    cout << "3. Delete task\n";
+    cout << "4. Quit\n";
+    cout << "Enter your choice (1-4): ";
+
+    cin >> choice;
+
+    switch (choice)
+    {
+      case 1:
+        addTask(tasks);
+        break;
+      case 2:
+        viewTasks(tasks);
+        break;
+      case 3:
+        deleteTask(tasks);
+        break;
+      case 4:
+        cout << "Goodbye!" << endl;
+        break;
+      default:
+        cout << "Invalid choice. Please enter a number between 1 and 4." << endl;
+    }
+  } 
+  while ( choice != 4);
+
+  return 0;
+}
